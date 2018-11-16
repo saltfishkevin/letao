758545cebd5cc197470fddcc9ba4cb564cd76b4a
saltfishkevin <510254711@qq.com>
2018-11-16 09:07:24 +0800

    公共功能退出功能和登录拦截完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,5 @@
+
+
 // NProgress.start();
 // setTimeout(function(){
 //   NProgress.done();
@@ -5,8 +7,58 @@
 
 $(document).ajaxStart(function(){
   NProgress.start();  
-})
+});
 
 $(document).ajaxStop(function(){
   NProgress.done();
+});
+
+$(function(){
+
+  //公共功能:
+  //1.导航点击切换功能
+
+  $(".lt_aside .category").click(function(){
+
+    $(this).next().stop().slideToggle();
+
+  });
+
+  //2.左侧菜单列表切换功能
+
+  $(".lt_topbar .icon_left").click(function(){
+
+    $(".lt_aside").toggleClass("hidemenu");
+
+    $(".lt_main").toggleClass("hidemenu");
+
+    $(".lt_topbar").toggleClass("hidemenu");
+
+  })
+
+  //3.退出功能
+  $(".lt_topbar .icon_right").click(function(){
+
+    $("#logoutModal").modal("show");
+
+  });
+
+  $("#logoutBtn").click(function(){
+    
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      dataType:"json",
+      success:function(info){
+        console.log(info);
+        if(info.success){
+          location.href="login.html";
+        }
+      }
+    });
+
+  })
+
+
+
 })
---
+++ public/back/js/isLogin.js
@@ -0,0 +1,15 @@
+//如果当前用户未登录，拦截到登录页
+$.ajax({
+  type:"get",
+  url:"/employee/checkRootLogin",
+  dataType:"json",
+  success:function(info){
+    console.log(info);
+    if(info.success){
+      console.log("用户已登录，继续访问");
+    }
+    if(info.error===400){
+      location.href="login.html";
+    }
+  }
+})
