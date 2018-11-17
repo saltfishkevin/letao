36648ba69b2703d0a5e303bb194de63320b42a06
saltfishkevin <510254711@qq.com>
2018-11-17 13:46:56 +0800

    基本列表渲染完成,文件上传功能完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -1,5 +1,6 @@
 $(function(){
 
+  //1.一进入页面渲染
   var currentPage=1;
   var pageSize=5;
 
@@ -18,9 +19,61 @@ $(function(){
         console.log(info);
         var htmlStr=template("secondTpl",info);
         $('tbody').html(htmlStr);
+
+        $("#paginator").bootstrapPaginator({
+          bootstrapMajorVersion:3,
+          totalPages:Math.ceil(info.total/info.size),
+          currentPage:info.page,
+          onPageClicked:function(a,b,c,page){
+            currentPage=page;
+            render();
+          }
+        })
       }
     })
   }
+
+  //2.点击按钮，显示添加模态框
+  $("#addBtn").click(function(){
+    $("#addModal").modal("show");
+
+    //发送ajax请求,获取下拉菜单一级分类数据
+    $.ajax({
+      type:"get",
+      url:"/category/queryTopCategoryPaging",
+      data:{
+        page:1,
+        pageSize:100
+      },
+      dataType:"json",
+      success:function(info){
+        console.log(info);
+        var htmlStr=template("dropdownTpl",info);
+        $(".dropdown-menu").html(htmlStr);
+      }
+    })
+  });
+
+  //3.给下拉菜单的所有a添加点击事件，通过事件委托注册
+  $(".dropdown-menu").on("click","a",function(){
+    // console.log(666);
+    var txt=$(this).text();
+    $("#dropdownText").text(txt);
+    console.log(txt);
+  })
+
+  //4.文件上传初始化
+  $("#fileupload").fileupload({
+
+    dataType:"json",
+    done:function(e,data){
+      console.log(data);
+      var result=data.result;
+      var picUrl=result.picAddr;
+      $("#imgBox img").attr("src",picUrl);
+    }
+
+  })
 
 
 })
