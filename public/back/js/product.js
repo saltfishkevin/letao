1ffd59ddea4f08f8cb9a05a68f326b1ab638722e
saltfishkevin <510254711@qq.com>
2018-11-19 11:02:50 +0800

    商品管理-清空数组修复

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -212,6 +212,8 @@ $(function(){
           //重置下拉按钮和图片内容
           $("#dropdownText").text("请选择二级分类");
           $("#imgBox img").remove();
+          //清空数组
+          picArr=[];
         }
       }
 
